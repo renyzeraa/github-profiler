e75fd1629f498493bc01a12bd7041ec1be3bab90
Renan Silva <renansilvaytB@gmail.com>
2024-04-26 21:36:54 -0300

    feat - classe de componente base e classe de componente html, montado html do header da pagina

--- app/main.js
+++ app/main.js
@@ -1,75 +1,209 @@
-function ComponenteBase() {
+/**
+ * Classe base de construção de um componente
+ * @param {String} sTipo
+ * @param {String} sName
+ * @returns {Componente}
+ */
+window['Componente'] = function (sTipo, sName) {
   this.obj
-  this.mobile = false
+  this.nome
 
   /**
-   * Define se é mobile
-   * @param {Boolean} bMobile
+   * Insere depois o componente a um alvo
+   * @param {HTMLElement} oAlvo
+   * @returns {HTMLElement}
    */
-  this.setMobile = function (bMobile) {
-    this.mobile = bMobile
+  this.appendTo = function (oAlvo) {
+    oAlvo.append(this.getObj())
+    return this
   }
 
   /**
-   * Retorna se é mobile
-   * @returns {Boolean}
+   * Insere antes o componente a um alvo
+   * @param {HTMLElement} oAlvo
+   * @returns {HTMLElement}
    */
-  this.getMobile = function () {
-    return this.mobile
+  this.prependTo = function (oAlvo) {
+    oAlvo.prepend(this.getObj())
+    return this.getObj()
   }
 
   /**
-   * Retorna o objeto
-   * @returns {Object}
+   * Cria o elemento HTML
+   * @param {String} sTipo
+   */
+  this.criaObjeto = function (sTipo) {
+    this.obj = document.createElement(sTipo)
+    this.obj.instance = this
+    return this
+  }
+
+  /**
+   * Define uma classe para o componente
+   * @param {String} sClass
+   * @returns {Componente}
+   */
+  this.addClass = function (sClass) {
+    this.getObj().classList.add(sClass)
+    return this
+  }
+
+  /**
+   * Remove uma classe do componente
+   * @param {String} sClass
+   * @returns {Componente}
+   */
+  this.removeClass = function (sClass) {
+    this.getObj().classList.remove(sClass)
+    return this
+  }
+
+  /**
+   * Define um atributo para o componente
+   * @param {String} sProp
+   * @param {Number|Boolean|Object} xValue
+   * @returns {Componente}
+   */
+  this.setAtributo = function (sProp, xValue) {
+    this.getObj().setAttribute(sProp, xValue)
+    return this
+  }
+
+  /**
+   * Define um objeto de atributos ao componente
+   * @param {Object} oAttr
+   * @returns {Componente}
+   */
+  this.setAtributos = function (oAttr) {
+    Object.keys(oAttr).forEach(sProp => {
+      this.setAtributo(sProp, oAttr[sProp])
+    })
+    return this
+  }
+
+  /**
+   * Retorna o valor de um atributo do componente
+   * @param {String} sAttr
+   */
+  this.getAtributo = function (sAttr) {
+    return this.getObj().getAttribute(sAttr)
+  }
+
+  /**
+   * Remove determinado atributo do componente
+   * @param {String} sAttr
+   * @returns {Componente}
+   */
+  this.removeAtributo = function (sAttr) {
+    this.getObj().removeAttribute(sAttr)
+    return this
+  }
+
+  /**
+   * Oculta o componente
+   * @returns {Componente}
+   */
+  this.hide = function () {
+    this.getObj().style.display = 'none'
+    return this
+  }
+
+  /**
+   * Mostra o componente
+   * @returns {Componente}
+   */
+  this.show = function () {
+    this.getObj().style.display = ''
+    return this
+  }
+
+  /**
+   * Retorna o elemento html principal
+   * @returns {HTMLElement}
    */
   this.getObj = function () {
     return this.obj
   }
 
   /**
-   * Define o objeto
-   * @param {Object} obj
+   * Retorna o nome do componente
+   * @returns {String}
    */
-  this.criaObject = function (sTipo) {
-    this.obj = document.createElement(sTipo)
+  this.getNome = function () {
+    return this.nome
   }
-}
-
-export default function Header() {
-  this.mobile = false
 
   /**
-   *
+   * Define um evento a ser disparado no componente
+   * @param {String} sEvent
+   * @param {Function} oFn
    */
-  function init() {
-    this.createElements()
+  this.on = function (sEvent, oFn) {
+    this.getObj().addEventListener(sEvent, oFn)
+    return this
   }
-
-  this.createElements = function () {
-    this.obj = document.createElement('nav')
-
-    // conteúdo do nav
-    const oNavContent = document.createElement('div')
-    this.obj.append(oNavContent)
-    oNavContent.classList.add('nav-github')
-
-    // logo git-hub
-    const oLogo = document.createElement('span')
-    oLogo.classList.add('nav-content')
-    oNavContent.append(oLogo)
-
-    // separador
-    const oSeparator = document.createElement('span')
-    oSeparator.classList.add('logo-github')
-    oNavContent.append(oSeparator)
-
-    // nome da página atual
-    const oNamePage = document.createElement('span')
-    oNamePage.classList.add('log-github')
-    oNavContent.append(oNamePage)
-  }
-
-  init.apply(this)
 
   return this
 }
+
+window['ComponenteHTML'] = function (sElement) {
+  /**
+   * Define o texto do componente
+   * @param {String} sText
+   * @returns {ComponenteHTML}
+   */
+  this.text = function (sText) {
+    this.getObj().textContent = sText
+    return this
+  }
+
+  /**
+   * Cria elemento html
+   */
+  function _init() {
+    Componente.apply(this)
+    this.criaObjeto(sElement)
+  }
+  _init.apply(this)
+  return this
+}
+ComponenteHTML.prototype = Object.create(Componente.prototype)
+
+window['oRoot'] = document.querySelector('.estrutura')
+
+function criaHeader() {
+  const oHeader = new ComponenteHTML('header')
+  oHeader.addClass('estrutura-header').appendTo(oRoot)
+
+  const oDivItens = new ComponenteHTML('div')
+  oDivItens
+    .addClass('estrutura-header-itens')
+    .addClass('container-global')
+    .appendTo(oHeader.getObj())
+
+  const oLogo = new ComponenteHTML('img')
+  oLogo
+    .addClass('estrutura-header-logo')
+    .appendTo(oDivItens.getObj())
+    .setAtributos({
+      src: './app/assets/logo_github.svg',
+      alt: 'logo github'
+    })
+
+  const oSeparator = new ComponenteHTML('span')
+  oSeparator
+    .addClass('estrutura-header-separator')
+    .text('/')
+    .appendTo(oDivItens.getObj())
+
+  const oSubject = new ComponenteHTML('span')
+  oSubject
+    .addClass('estrutura-header-subject')
+    .text('Profile')
+    .appendTo(oDivItens.getObj())
+}
+
+function iniciaSistema() {
+  criaHeader()
+}
+iniciaSistema()
